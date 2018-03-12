6edafe9a72e5c57854f183036d16813e80bc4a52
LHN0428 <37294809+LHN0428@users.noreply.github.com>
2018-03-12 20:29:00 +0800

    2018-3-12

--- USER/Objects/system_skeaz1284.d
+++ USER/Objects/system_skeaz1284.d
@@ -1,7 +1,7 @@
 .\objects\system_skeaz1284.o: ..\CORE\system_SKEAZ1284.c
-.\objects\system_skeaz1284.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_skeaz1284.o: D:\KeilPack\Keil\Kinetis_KEAxx_DFP\1.3.0\Device\Include\SKEAZ1284.h
-.\objects\system_skeaz1284.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm0plus.h
-.\objects\system_skeaz1284.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
-.\objects\system_skeaz1284.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
-.\objects\system_skeaz1284.o: D:\KeilPack\Keil\Kinetis_KEAxx_DFP\1.3.0\Device\Include\system_SKEAZ1284.h
+.\objects\system_skeaz1284.o: D:\KEIL5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_skeaz1284.o: D:\KEIL5\ARM\PACK\Keil\Kinetis_KEAxx_DFP\1.3.0\Device\Include\SKEAZ1284.h
+.\objects\system_skeaz1284.o: D:\KEIL5\ARM\CMSIS\Include\core_cm0plus.h
+.\objects\system_skeaz1284.o: D:\KEIL5\ARM\CMSIS\Include\core_cmInstr.h
+.\objects\system_skeaz1284.o: D:\KEIL5\ARM\CMSIS\Include\core_cmFunc.h
+.\objects\system_skeaz1284.o: D:\KEIL5\ARM\PACK\Keil\Kinetis_KEAxx_DFP\1.3.0\Device\Include\system_SKEAZ1284.h
